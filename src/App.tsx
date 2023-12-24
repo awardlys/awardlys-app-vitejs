4f8f6f8a0b8c0bf9a963e5c11adf093e3e77fab8
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2023-12-24 18:13:16 -0300

    feat(src): settings layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,80 @@
+import {
+  MenuFoldOutlined,
+  MenuUnfoldOutlined,
+  UploadOutlined,
+  UserOutlined,
+  VideoCameraOutlined,
+} from "@ant-design/icons";
+import { Button, Layout, Menu, theme } from "antd";
+import { useState } from "react";
+
+const { Header, Sider, Content } = Layout;
+
 export function App() {
-  return <div>Hello world</div>;
+  const [collapsed, setCollapsed] = useState(false);
+  const {
+    token: { colorBgContainer, borderRadiusLG },
+  } = theme.useToken();
+  return (
+    <Layout style={{ height: "100vh" }}>
+      <Sider trigger={null} collapsible collapsed={collapsed}>
+        <div className="demo-logo-vertical">
+          <Button
+            type="text"
+            icon={collapsed ? <MenuUnfoldOutlined /> : <MenuFoldOutlined />}
+            onClick={() => setCollapsed(!collapsed)}
+            style={{
+              fontSize: "16px",
+              width: 64,
+              height: 64,
+              color: "white",
+            }}
+          />
+
+          <span
+            style={{ color: "white", display: collapsed ? "none" : "inline" }}
+          >
+            Awardlys
+          </span>
+        </div>
+
+        <Menu
+          theme="dark"
+          mode="inline"
+          defaultSelectedKeys={["1"]}
+          items={[
+            {
+              key: "1",
+              icon: <UserOutlined />,
+              label: "Premiações",
+            },
+            {
+              key: "2",
+              icon: <VideoCameraOutlined />,
+              label: "Categorias",
+            },
+            {
+              key: "3",
+              icon: <UploadOutlined />,
+              label: "Jogos",
+            },
+          ]}
+        />
+      </Sider>
+      <Layout>
+        <Header style={{ padding: 0, background: colorBgContainer }}></Header>
+        <Content
+          style={{
+            margin: "24px 16px",
+            padding: 24,
+            minHeight: 280,
+            background: colorBgContainer,
+            borderRadius: borderRadiusLG,
+          }}
+        >
+          Content
+        </Content>
+      </Layout>
+    </Layout>
+  );
 }
