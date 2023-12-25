93bfa7f3ddf1ac1d5fe8dcec51434cefe7d8edc6
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2023-12-25 10:34:28 -0300

    feat(*): create profile icon

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -1,7 +1,37 @@
+import { Input, Table } from "antd";
+import { SearchProps } from "antd/es/input";
+import { useState } from "react";
+import { awards } from "../../data";
+import "./style.css";
+
+const {Search} = Input;
+
 export function Categories() {
+  const [search, setSearch] = useState(awards);
+  const onSearch: SearchProps["onSearch"] = (value) => {
+    const search = awards.filter((item) =>
+      item.title.toLocaleLowerCase().includes(value.toLocaleLowerCase())
+    );
+    setSearch(search);
+  };
+
   return (
-    <div>
-      <p>Categorias</p>
+    <div className="container-categories">
+      <Search placeholder="Pesquise" onSearch={onSearch} />
+      <Table
+        dataSource={search}
+        columns={[
+          {
+            dataIndex: "title",
+            title: "Nome",
+            sorter: (a, b) => a.title.localeCompare(b.title),
+          },
+          {
+            dataIndex: "subTitle",
+            title: "Descrição",
+          },
+        ]}
+      />
     </div>
   );
 }
