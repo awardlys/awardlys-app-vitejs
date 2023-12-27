e2130096f76abb00aabd6ba900880e0b7d232407
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2023-12-27 18:27:46 -0300

    feat(home): dark mode

---
+++ src/pages/home/components/content/index.tsx
@@ -0,0 +1,13 @@
+export function Content() {
+  return (
+    <div className="content-container">
+      <h2>Awardlys</h2>
+      <h3>Sobre o Projeto</h3>
+      <p>
+        O Awardlys App é uma aplicação web construída com Vitejs e Ant Design,
+        proporcionando uma experiência dinâmica e elegante para os usuários
+        votarem nas melhores categorias.
+      </p>
+    </div>
+  );
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,10 +1,21 @@
+import { useState } from "react";
+import { Content } from "./components/content";
 import { Header } from "./components/header";
 import "./style.css";
+import { Button } from "antd";
+import { BulbFilled, BulbTwoTone } from "@ant-design/icons";
 
 export function Home() {
+  const [mode, setMode] = useState(false);
   return (
-    <main className="container-home">
+    <main className={`container-home ${mode ? "dark-mode" : ""}`}>
+      <Button
+        type={mode ? "default" : "primary"}
+        icon={mode ? <BulbTwoTone /> : <BulbFilled />}
+        onClick={() => setMode(!mode)}
+      />
       <Header />
+      <Content />
     </main>
   );
 }
