32956cdcbcda88ba80fc03d48ebc0e5e6da5514c
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2023-12-27 16:48:06 -0300

    feat(categories): some settings

--- src/pages/categories/index.tsx
+++ src/pages/categories/index.tsx
@@ -6,35 +6,47 @@ import "./style.css";
 import { useCategory } from "./hooks";
 import { TableCategory } from "./components/table";
 import { TooltipCategories } from "./components/tooltip";
+import { useStoreCategory } from "./store";
+import { useState } from "react";
 
 const { Search } = Input;
 
 export function Categories() {
-  const { onSearch, setIsModalOpen } = useCategory()
+  const { setIsModalOpen } = useStoreCategory();
+  const [search, setSearch] = useState("");
+  const { onSearch } = useCategory();
 
   return (
     <main className="container-categories">
       <Row gutter={[24, 24]} justify={"end"}>
         <Col>
-          <Button type="primary" className="button-new-category" onClick={() => setIsModalOpen(true)} icon={<PlusOutlined />}>
+          <Button
+            type="primary"
+            className="button-new-category"
+            onClick={() => setIsModalOpen(true)}
+            icon={<PlusOutlined />}
+          >
             Novo
           </Button>
         </Col>
       </Row>
       <Row gutter={[24, 24]}>
         <Col sm={24} md={16} lg={12} xl={8}>
-          <Search placeholder="Pesquise pelo nome" onSearch={onSearch} />
+          <Search
+            placeholder="Busque pelo nome"
+            onSearch={(value) => setSearch(value)}
+          />
         </Col>
-        <Col >
+        <Col>
           <TooltipCategories />
         </Col>
       </Row>
       <Row gutter={[24, 24]}>
         <Col span={24}>
-          <TableCategory />
+          <TableCategory search={onSearch(search)} />
         </Col>
       </Row>
       <FormModal />
-    </main >
+    </main>
   );
 }
