a30560260f86e44570a5d8cafb1c54fbc2fd81c7
Rivaldo Mascarenhas <rivaldomascarenhas007@gmail.com>
2023-12-27 15:45:48 -0300

    feat(hooks): created function to submit  form data

--- src/pages/categories/hooks/useFormCategory.ts
+++ src/pages/categories/hooks/useFormCategory.ts
@@ -1,17 +1,42 @@
 import { Form } from "antd";
 import { useState } from "react";
 import { ValueProps } from "../../../types";
+import {
+  postCategory,
+  updateCategory,
+} from "../../../services/http/categories";
+import { useStoreCategory } from "../store";
 
 export function useFormCategory() {
+  const {
+    isModalOpen,
+    setIsModalOpen,
+    setTryAgain,
+    categoryEdit,
+    setCategoryEdit,
+  } = useStoreCategory();
   const [form] = Form.useForm<ValueProps>();
-  const [submittable, setSubmittable] = useState(false);
-  const onFinish = (value: ValueProps) => {
-    console.log(value);
+  const [submitTable, setSubmitTable] = useState(false);
+  const onFinish = async (value: ValueProps) => {
+    setSubmitTable(true);
+    if (categoryEdit?.id) {
+      await updateCategory(categoryEdit.id, value);
+    } else {
+      await postCategory(value);
+    }
+    setCategoryEdit(null);
+    setSubmitTable(false);
+    setIsModalOpen(false);
+    setTryAgain(true);
+    form.resetFields();
   };
   return {
     form,
-    setSubmittable,
-    submittable,
+    submitTable,
     onFinish,
+    isModalOpen,
+    setIsModalOpen,
+    categoryEdit,
+    setCategoryEdit,
   };
 }
